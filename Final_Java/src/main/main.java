9f7e2e9256005d407b8afdadcf31267d23a21fc6
LAB2-15\Lab 02 - 12 <trunghaunguyen160826@gmail.com>
2025-12-10 11:15:46 +0700

    hau test 2

--- Final_Java/src/main/main.java
+++ Final_Java/src/main/main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for (int i = 0; i < 10; i++) {
-			System.out.println(i);
+			System.out.println(i+1);
 		}
 	}
 
